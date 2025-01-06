84f1789b3dff93058ff2c1dd35682dadfb0ffe14
khg0418 <rlagyrud222@gmail.com>
2025-01-06 12:42:58 +0900

    add JavaTest

---
+++ JavaTest/src/test1/test01.java
@@ -0,0 +1,23 @@
+package test1;
+
+/*
+ * 날짜: 2025/01/06
+ * 이름: 김효경
+ * 내용: 자바 자료형 연습문제
+ */
+public class test01 {
+	public static void main(String[] args) {
+		
+		var1 = 'A';
+		var2 = 100;
+		var3 = 2147483648L;
+		var4 = 3.14f;
+		var5 = 3.14159;
+		var6 = true;
+		var7 = false;
+		var8 = "hello";
+		var9 = null;
+				
+	}
+
+}
