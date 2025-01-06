95f55cecc0e13cc54207d85c661d0552d19d9bbf
khg0418 <rlagyrud222@gmail.com>
2025-01-06 11:58:03 +0900

    add ch04

---
+++ ch04/src/sub2/MethodTypeTest.java
@@ -0,0 +1,63 @@
+package sub2;
+/**
+ * 날짜: 2024/01/06
+ * 이름: 김효경
+ * 내용: Java 메서드 실습하기
+ */
+public class MethodTypeTest {
+	
+	public static void main(String[] args) {
+		
+		// 메서드 호출
+		double y1 = type1(1.2); // 인자값: 메서드를 호출할 때 전달하는 값
+		double y2 = type1(2.3);
+		
+		System.out.println("y1: " + y1);
+		System.out.println("y2: " + y2);
+		
+		type2(true);
+		type2(false);
+		
+		boolean result = type3();
+		System.out.println("result: " + result);
+		
+		type4();
+	}
+	
+	// 타입1: 매개변수 o, 반환값 o
+	public static double type1(double r) {
+		
+		double y = (r * r) + 3.14;
+		return y;
+		
+	}
+	// 타입2: 매개변수 o, 반환값 x
+	public static void type2(boolean status) {
+		
+		if(status) {
+			System.out.println("참 입니다.");
+		}else {
+			System.out.println("거짓 입니다.");
+		}
+		
+	}
+	// 타입3: 매겨변수 x, 반환값 o
+	public static boolean type3() {
+		
+		int num1 = 1;
+		int num2 = 2;
+		
+		if(num1 > num2) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	// 타입4: 매개변수 x, 반환값 x
+	public static void type4() {
+		
+		double result = type1(2.3);
+		System.out.println("반지름 2.3인 원의 넓이: " + result);
+	}
+
+}
