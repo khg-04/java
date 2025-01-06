dac5ac629b89cf209bdfed1291747efd1c7e688b
khg0418 <rlagyrud222@gmail.com>
2025-01-06 12:42:35 +0900

    add CodingTest

---
+++ CodingTest/src/step1/Test01.java
@@ -0,0 +1,12 @@
+package step1;
+// package 밑부터 복사
+//---------------
+// https://www.acmicpc.net/step
+// public class Main으로 고쳐서 제출
+public class Test01 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
