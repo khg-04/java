3e41a03cb2f05ff371cee18b3ec8102daf8b2f6d
khg0418 <rlagyrud222@gmail.com>
2025-01-08 17:44:28 +0900

    add JavaTest

--- JavaTest/src/test1/test06.java
+++ JavaTest/src/test1/test06.java
@@ -1,5 +1,26 @@
 package test1;
 
+import java.util.Scanner;
+
+/*
+ * 날짜: 2025/01/08
+ * 이름: 김효경
+ * 내용: 자바 삼항연산자 연습문제
+ */
 public class test06 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("x값 입력: ");
+		int x = sc.nextInt();
+		
+		System.out.println("y값 입력: ");
+		int y = sc.nextInt();
+		
+		String result = x > y ? "x가 더 큽니다." : "y가 더 큽니다.";
+		
+		System.out.println(result);
+	}
 
 }
--- JavaTest/src/test1/test08.java
+++ JavaTest/src/test1/test08.java
@@ -1,5 +1,29 @@
 package test1;
 
+/*
+ * 날짜: 2025/01/08
+ * 이름: 김효경
+ * 내용: 자바 반복문 연습문제
+ */
 public class test08 {
+	public static void main(String[] args) {
+		
+		int n = 5;
+		
+		for(int i = 0; i<n; i++) {
+
+			for(int j=n-1; j>i ; j--) {
+				System.out.print(" ");
+	
+			}
+	
+			for(int j=0; j<=i*2; j++) {
+				System.out.print("*");				
+	
+			}
+			
+			System.out.println("\n");
+		}
+	}
 
 }
