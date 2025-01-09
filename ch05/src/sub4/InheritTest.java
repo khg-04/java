d5d57fa11e4b6ad94102ec4b1528da968d034eee
khg0418 <rlagyrud222@gmail.com>
2025-01-09 17:48:25 +0900

    add ch05

--- ch05/src/sub4/InheritTest.java
+++ ch05/src/sub4/InheritTest.java
@@ -25,9 +25,39 @@ class Child extends Parent {
 	
 	public Child(int num1, int num2, int num3, int num4) {
 		super(num1, num2); // 부모클래스 생성자 호출
+		this.num3 = num3;
+		this.num4 = num4;
+	}
+	
+	public int minus() {
+		return num3 - num4;
 	}
 }
 
 public class InheritTest {
+	public static void main(String[] args) {
+		
+		// 상속 객체 생성
+		Child child = new Child(1, 2, 3, 4);
+		int result1 = child.plus();
+		int result2 = child.minus();
+		
+		System.out.println("result1: " + result1);
+		System.out.println("result2: " + result2);
+		
+		// Car 상속 객체 생성
+		Sedan sonata = new Sedan("소나타", "흰색", 0, 2000);
+		sonata.speedUp(80);
+		sonata.turbo();
+		sonata.speedDown(20);
+		//sonata.name = "아반테";
+		sonata.show();
+		
+		Truck bongo = new Truck("봉고", "남색", 0, 0);
+		bongo.load(100);
+		bongo.speedUp(60);
+		bongo.show();
+	
+	}
 
 }
---
+++ ch05/src/sub4/Person.java
@@ -0,0 +1,11 @@
+package sub4;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	
+	public Person(String name, int age) {
+		
+	}
+}
