f29ac66de2826a0366238db8331faac7a3d17e79
khg-04 <rlagyrud222@gmail.com>
2025-01-22 23:38:05 +0900

    add ThisisJava

--- ThisisJava/src/ch02/sec13/ScannerExample.java
+++ ThisisJava/src/ch02/sec13/ScannerExample.java
@@ -7,7 +7,28 @@ public class ScannerExample {
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("x 값 입력: ");
-		String
+		String strX = scanner.nextLine(); 
+		// nextLine(): Enter를 누르면 입력된 모든 내용을 문자열로 읽음
+		int x = Integer.parseInt(strX);
+		
+		System.out.println("y 값 입력: ");
+		String strY = scanner.nextLine(); 
+		int y = Integer.parseInt(strY);
+		
+		int result = x + y;
+		System.out.println("x + y: " + result);
+		System.out.println();
+		
+		while(true) {
+			System.out.println("입력 문자열: ");
+			String data = scanner.nextLine();
+			if(data.equals("q")) {
+				break;
+			}
+			System.out.println("출력 문자열: " + data);
+			System.out.println();
+		}
+		System.out.println("종료");
 	}
 
 }
