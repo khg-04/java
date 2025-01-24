81bccb492870458a0b0979ebf407d0f568aeb95d
khg-04 <rlagyrud222@gmail.com>
2025-01-24 12:49:43 +0900

    add CodingTest

--- CodingTest/src/step2/Test06.java
+++ CodingTest/src/step2/Test06.java
@@ -1,8 +1,32 @@
 package step2;
 
+import java.util.Scanner;
+
 public class Test06 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int hour = sc.nextInt();
+		int min = sc.nextInt();
+		int time = sc.nextInt();
 		
+		sc.close();
+		
+		int total_min = min + time;
+		boolean vail = true;
+		
+		while(vail) {
+			if(total_min >= 60) {
+				hour++;
+				total_min = total_min - 60;
+				if(hour == 24) {
+					hour = 0;
+				}
+			} else {
+				vail = false;
+			
+			System.out.println(hour + " " + total_min);
+			}
+		}
 	}
 
 }
--- CodingTest/src/step2/Test07.java
+++ CodingTest/src/step2/Test07.java
@@ -1,5 +1,26 @@
 package step2;
 
-public class Test07 {
+import java.util.Scanner;
 
+public class Test07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        
+        sc.close();
+        
+        if (x == y && y == z) {  // 세 수가 모두 같으면
+            System.out.println(10000 + x * 1000);
+        } else if (x == y || y == z || z == x) {  // 두 수가 같으면
+            if (x == y || x == z) {
+                System.out.println(1000 + x * 100);
+            } else {
+                System.out.println(1000 + y * 100);
+            }
+        } else {  // 세 수가 모두 다르면
+            System.out.println(Math.max(Math.max(x, y), z) * 100);
+        }
+    }
 }
