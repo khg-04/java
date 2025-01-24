5ff7853af8a0e1c7aa48c80c3e596fd4e0fd5cdb
khg-04 <rlagyrud222@gmail.com>
2025-01-24 17:38:30 +0900

    add CodingTest

--- CodingTest/src/step3/Test01.java
+++ CodingTest/src/step3/Test01.java
@@ -1,5 +1,17 @@
 package step3;
 
+import java.util.Scanner;
+
 public class Test01 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		for(int i=1 ; i <= 9 ; i++) {
+			int result = N * i;
+			System.out.println(N + " * " + i + " = " + result);
+		}
+	}
 
 }
--- CodingTest/src/step3/Test02.java
+++ CodingTest/src/step3/Test02.java
@@ -1,5 +1,8 @@
 package step3;
 
 public class Test02 {
+	public static void main(String[] args) {
+		
+	}
 
 }
